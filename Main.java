9fde0adeab36de84e1a363c47982c199fdfa5234
IliaMarcoZecevic <ilia.zecevic@gmail.com>
2024-10-04 19:11:31 +0200

    added description for colour spray, simplified the spell check function

--- Main.java
+++ Main.java
@@ -3,14 +3,9 @@ import util.DiceNotation;
 class Main {
 
     public static void main(String[] args) {
-        System.out.println(DiceNotation.addDice("4d12+1d2+7", "1d6+2d2+5"));
-        Creature a = new Creature();
-        a.statusEffects.add(new StatusEffectInstance(StatusEffect.PETRIFIED, 3, 3));
-        a.inventory.add(new ItemStack(new Item(ItemType.WOODEN_SWORD), 2));
-        System.out.println(a.inventory.get(0));
-        System.out.println(StatusEffect.BLINDED.DESCRIPTION);
-        System.out.println(Spell.get("Eldritch Blast").getDescription(0, 5));
-        //testSpells();
+        // System.out.println(Spell.get("Eldritch Blast").getDescription(0, 5));
+        testSpell("Colour Spray");
+        // testSpells();
     }
 
     static void testSpells() {
@@ -25,4 +20,14 @@ class Main {
         }
     }
 
+    static void testSpell(String spellName) {
+        Spell spell = Spell.get(spellName);
+        int testlevel;
+        System.out.println(spell.NAME + " level " + spell.LEVEL + ":\n" + spell.getDescription(spell.LEVEL, 1));
+        if (spell.FORMULAS.size() > 0) {
+            testlevel = spell.LEVEL + 1;
+            System.out.println(spell.NAME + " level " + testlevel + ":\n" + spell.getDescription(testlevel, 1));
+        }
+        System.out.println("###\n");
+    }
 }
